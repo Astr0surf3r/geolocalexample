9bb4f36adfe9431e73cfaa0b6ce29c13c07a6843
Astr0surf3r <astr0surf3r@gmail.com>
2013-11-06 13:51:47 -0800

    rilevazione dati utente

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,10 @@
 class Visit < ActiveRecord::Base
   attr_accessible :city, :country, :ipaddress
+  
+  geocoded_by :ip_address,
+  
+     :ip_address => :ip_lookup
+  
+  after_validation :geocode
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Geo::Application.routes.draw do
   resources :visits
 
-
+  root :to => 'visits#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
