8138b4552afcbf9958b6e108a2d5eb87ec781747
Astr0surf3r <astr0surf3r@gmail.com>
2013-11-08 14:23:40 -0800

    remove the geocoded_by from Visit Controller

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,12 +1,12 @@
 class Visit < ActiveRecord::Base
   attr_accessible :city, :country, :ipaddress
   
-#=begin
+=begin
   geocoded_by :ipaddress,
   
      :city => :city, :country => :country
   
   after_validation :geocode
-#=end
+=end
 
 end
